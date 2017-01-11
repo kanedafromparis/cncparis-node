6ff3eb018357a44412c4be06f8c0183fcb46b790
kanedafromparis <kanedafromparis@gmail.com>
2017-01-11 17:44:20 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.get('/', (req, res) => res.send(`
+  <html>
+          <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" >
+        <title>I love Openshift/kubernetes</title>
+        <meta http-equiv="refresh" content="5" >
+    <head>
+    
+    </head>
+  <body>
+    <h1>Hello</h1>
+    <img src="/public/logo_cncf_0001.png" />
+  </body>
+  </html>
+`));
+
+app.use('/public', express.static(path.join(__dirname, 'public')))
+
+app.listen(8080);
