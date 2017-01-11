05cfae71f950b56a5b256b796e8232462ece8d4d
kanedafromparis <kanedafromparis@gmail.com>
2017-01-11 17:50:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+
 app.get('/', (req, res) => res.send(`
   <html>
           <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" >
@@ -18,4 +22,4 @@ app.get('/', (req, res) => res.send(`
 
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
-app.listen(8080);
+app.listen(server_port, server_ip_address);
