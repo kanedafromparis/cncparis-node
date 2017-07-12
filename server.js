ab762af5d7cf29d70a1a93b5ca4166fc275c5878
kanedafromparis <kanedafromparis@gmail.com>
2017-07-12 17:45:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => res.send(`
     </head>
   <body>
     <h1>Hello</h1>
-    <!-- yet another silly modification -->
+    <!-- yet another so silly modification -->
     <img src="/public/logo_cncf_0003.png" />
   </body>
   </html>
