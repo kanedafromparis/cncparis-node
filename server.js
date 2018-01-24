3f9f282a20841d086e3e2e5e45f871660ce6f34b
Charles Sabourdin <kanedafromparis@gmail.com>
2018-01-24 10:52:22 +0100

    put cookie seting into variable via the env variable OPENSHIFT_COOKIE_NAME

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ const path = require('path');
 const app = express();
 var Log = require('log')
   , log = new Log('info');
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_setcookie = process.env.OPENSHIFT_COOKIE_NAME;
 
+log.info('process.env.OPENSHIFT_COOKIE_NAME :'+process.env.OPENSHIFT_COOKIE_NAME);
 
 app.get('/', (req, res) => {
     log.info('ping :'+JSON.stringify(req.headers));
-    res.cookie('rememberme', '1', { expires: new Date(Date.now() + 900000)});
+    
+    if(typeof server_setcookie !== 'undefined' && server_setcookie){
+      res.cookie('server_setcookie', '1', { expires: new Date(Date.now() + 900000)});
+    }
     res.send(`
   <html>
           <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" >
